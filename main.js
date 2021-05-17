5339fd84122fca6d6d886193af56c4ab8df44252
user <icupayliedie@gmail.com>
2021-05-17 20:36:22 +0300

    Подвязал фреймворки через <script> основных файлов. Не знаю правильно ли это, пока что попробую так. Сам Vue работает (пока что)

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-var app = new Vue({
+let app = new Vue({
     el: '#app',
     data: {
         message: "Test"
     }
 })
 
-moment().format('MMMM Do YYYY, h:mm:ss a');
+let empdata = new Vue({
+    el: '#empdata',
+    Emp: {
+        fio: {
+
+        },
+        pass_ser: {
+
+        },
+        pass_no: {
+
+        },
+        pass_dt: {
+
+        }
+    }
+    }
+)
